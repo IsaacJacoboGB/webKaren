e8b340a0f212cb937cabfece4088955b71f554b5
Isaac Jacobo Galindo Barbina <isaacgalindobag@gmail.com>
2024-10-13 01:20:47 -0600

    changes in main and method

--- js/titleChange.js
+++ js/titleChange.js
@@ -1,3 +1,32 @@
+// document.addEventListener('DOMContentLoaded', function () {
+// 	// Cargar el contenido del header desde header.html
+// 	fetch('/header.html')
+// 		.then((response) => response.text())
+// 		.then((data) => {
+// 			// Insertar el header en el placeholder
+// 			document.getElementById('header-placeholder').innerHTML = data;
+
+// 			// Después de insertar el header, cambiar el título dinámicamente
+// 			const pathname = window.location.pathname;
+// 			const titleElement = document.querySelector('.titleWelcome h2');
+
+// 			if (titleElement) {
+// 				// Verificar que el elemento existe
+// 				// Lógica para cambiar el título según la página
+// 				if (pathname.includes('coatetelco.html')) {
+// 					titleElement.textContent = '¡Coatetelco!';
+// 				} else if (pathname.includes('laspilas.html')) {
+// 					titleElement.textContent = '¡Las Pilas!';
+// 				} else if (pathname.includes('olintepec.html')) {
+// 					titleElement.textContent = '¡Olintepec!';
+// 				} else if (pathname.includes('yautepec.html')) {
+// 					titleElement.textContent = '¡Yautepec!';
+// 				} else {
+// 					titleElement.textContent = '¡Bienvenidos a Morelos!';
+// 				}
+// 			}
+// 		});
+// });
 document.addEventListener('DOMContentLoaded', function () {
 	// Cargar el contenido del header desde header.html
 	fetch('/header.html')
@@ -11,15 +40,17 @@ document.addEventListener('DOMContentLoaded', function () {
 			const titleElement = document.querySelector('.titleWelcome h2');
 
 			if (titleElement) {
-				// Verificar que el elemento existe
+				// Remover la extensión .html si existe
+				const cleanedPathname = pathname.replace('.html', '');
+
 				// Lógica para cambiar el título según la página
-				if (pathname.includes('coatetelco.html')) {
+				if (cleanedPathname.includes('coatetelco')) {
 					titleElement.textContent = '¡Coatetelco!';
-				} else if (pathname.includes('laspilas.html')) {
+				} else if (cleanedPathname.includes('laspilas')) {
 					titleElement.textContent = '¡Las Pilas!';
-				} else if (pathname.includes('olintepec.html')) {
+				} else if (cleanedPathname.includes('olintepec')) {
 					titleElement.textContent = '¡Olintepec!';
-				} else if (pathname.includes('yautepec.html')) {
+				} else if (cleanedPathname.includes('yautepec')) {
 					titleElement.textContent = '¡Yautepec!';
 				} else {
 					titleElement.textContent = '¡Bienvenidos a Morelos!';
